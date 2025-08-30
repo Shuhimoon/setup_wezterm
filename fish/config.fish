162e2a50d3f7bf70dc0d8b4a63d1555db58f7105
shuhi <128314725+Shuhimoon@users.noreply.github.com>
2025-08-30 17:58:03 +0800

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+## config.fish
+
+# 檢查檔案是否存在並匯入必要的環境變數
+if status is-interactive
+    set -l shell_rc
+    if test -f "$HOME/.bashrc"
+        set shell_rc "$HOME/.bashrc"
+    else if test -f "$HOME/.zshrc"
+        set shell_rc "$HOME/.zshrc"
+    end
+
+    if set -q shell_rc
+        # 只匯入 PATH 和必要的環境變數
+        for line in (bash -c "source $shell_rc; env | grep -E '^(PATH|HOME|USER|CARGO_HOME|RUSTUP_HOME|NVM_DIR|GOPATH|GOROOT)='")
+            set -l key_value (string split -m 1 '=' $line)
+            set -gx $key_value[1] $key_value[2]
+        end
+    end
+end
+
+# Rust/Cargo 路徑
+set -gx PATH $PATH ~/.cargo/bin
